02a59d7812bc500aadad6bb6e2951bb07d077651
Anastasia-Sablina <1020030829@qq.com>
2022-07-18 00:54:48 +0800

    Site updated: 2022-07-18 00:54:44

--- HTML/jy901/navtreedata.js
+++ HTML/jy901/navtreedata.js
@@ -27,6 +27,7 @@ var NAVTREE =
   [ "DIY-JY901 Docs", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
       [ "Class Members", "functions.html", [
         [ "All", "functions.html", null ],
         [ "Variables", "functions_vars.html", null ]
